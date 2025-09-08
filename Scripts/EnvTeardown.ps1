77bbea8c59f9c8b329a108cc88c080343def3af8
Calvin Aristene <calvinaristene1@outlook.com>
2025-09-08 15:48:27 -0400

    Update EnvTeardown.ps1

--- Scripts/EnvTeardown.ps1
+++ Scripts/EnvTeardown.ps1
@@ -1,10 +1,8 @@
-Local Environment Cleanup Script
-
 # Environment Teardown Script
-Write-Host "'n Starting teardown of environment..."
+Write-Host "`nStarting teardown of environment..."
 
 # Define users and folders
-$users = @("Mwright, "Ljohnson", "Dphillips", "Bstewart", "Tgreen")
+$users = @("Mwright", "Ljohnson", "Dphillips", "Bstewart", "Tgreen")
 $basePath = "C:\CompanyData"
 $folders = @("Reports", "Monitoring", "Backups")
 $backupScript = "C:\Scripts\Backup.ps1"
@@ -13,32 +11,37 @@ $taskName = "DailyBackup"
 # Remove scheduled task
 try {
     Unregister-ScheduledTask -TaskName $taskName -Confirm:$false -ErrorAction Stop
-    Write-Host "Scheduled task 'taskName' removed."
+    Write-Host "Scheduled task '$taskName' removed."
 } catch {
-    Write-Warning " Scheduled Task 'taskName' not found or could not be removed. "
+    Write-Warning "Scheduled Task '$taskName' not found or could not be removed."
+}
 
 # Remove backup script
-if (Test-path $backupScrupt) {
+if (Test-Path $backupScript) {
     Remove-Item $backupScript -Force
     Write-Host "Backup script removed."
 } else {
-    Write-Warning "Backup script not found. "
+    Write-Warning "Backup script not found."
 }
 
-#Remove folders
+# Remove folders
 foreach ($folder in $folders) {
     $path = Join-Path $basePath $folder
     if (Test-Path $path) {
-        Remove-Item -Path $basePath -Recurse -Force
-        Write-Host "Base folder removed"
+        Remove-Item -Path $path -Recurse -Force
+        Write-Host "Folder '$folder' removed."
+    } else {
+        Write-Warning "Folder '$folder' not found."
+    }
 }
+
 # Remove users
 foreach ($user in $users) {
     try {
         Remove-LocalUser -Name $user -ErrorAction Stop
-        Write-Host "User 'user removed."
+        Write-Host "User '$user' removed."
     } catch {
-        Write-Warning " User "$user' not found or could not removed."
+        Write-Warning "User '$user' not found or could not be removed."
     }
 }
-Write-Host "'n Environment teardown complete." 
+
